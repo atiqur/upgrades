ebdf8b66ae86b63463df4fe2d075e952d661facf
Atiqur Rahman <atiqur.india@gmail.com>
2022-04-07 13:04:12 +0530

    Deploy proxy

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,3 +1,4 @@
+import eth_utils
 from brownie import network, accounts, config
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache", "mainnet-fork"]
@@ -10,4 +11,13 @@ def get_account(index=None, id=None):
         return accounts[0]
     if id:
         return accounts.load(id)
-    return accounts.add(config["wallets"]["from_key"])
+    if network.show_active() in config["networks"]:
+        return accounts.add(config["wallets"]["from_key"])
+    return None
+
+
+# initializer=box.store, 1
+def encode_function_data(initializer=None, *args):
+    if len(args) == 0 or not initializer:
+        return eth_utils.to_bytes(hexstr="0x")
+    return initializer.encode_input(*args)
